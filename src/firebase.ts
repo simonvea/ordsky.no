c53934f13dfc90552129b7d89c778f82440bc697
Simon Opheim <simon.opheim@gmail.com>
2020-10-07 17:40:44 +0200

    fix: make new API key secret

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,9 +1,17 @@
 import * as firebase from 'firebase/app';
 import 'firebase/analytics';
-import { firebaseConfig } from './firebase.config';
 
 if (process.env.NODE_ENV === 'production') {
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp({
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: 'ordsky-41e2e.firebaseapp.com',
+    databaseURL: 'https://ordsky-41e2e.firebaseio.com',
+    projectId: 'ordsky-41e2e',
+    storageBucket: 'ordsky-41e2e.appspot.com',
+    messagingSenderId: '815660490211',
+    appId: '1:815660490211:web:91e29edbb14f52e29d54d6',
+    measurementId: 'G-TC27H6R5BX',
+  });
 }
 
 export const analytics =
