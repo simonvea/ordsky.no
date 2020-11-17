15c056eaefeb997110703c88d96b5c0a20357aee
Simon Opheim <simon.opheim@gmail.com>
2020-11-17 20:53:18 +0100

    feat: add text to explain leader role

---
+++ src/components/atoms/TextContainer.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const TextContainer = styled.div`
+  font-size: 1.125rem;
+  line-height: 1.5625rem;
+  width: calc(100% - 40px);
+  max-width: 600px;
+  text-rendering: optimizeLegibility;
+`;
--- src/features/session/WaitScreen.tsx
+++ src/features/session/WaitScreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Button, SecondaryButton } from '../../components/atoms/Button';
+import { TextContainer } from '../../components/atoms/TextContainer';
 import { Title } from '../../components/atoms/Title';
 import { Spinner } from '../../components/Spinner';
 
@@ -30,6 +31,21 @@ export const WaitScreen: React.FC<WaitScreenProps> = ({
 
   return (
     <>
+      {isAdmin && (
+        <TextContainer>
+          <p>Del denne koden med de du ønsker å lage en ordsky sammen med.</p>
+          <p>
+            Når de som skal legge inn ord har lagt inn ord, kan du trykke på
+            &quot;Lag ordsky&quot; for å lage en ordsky av alle ordene som er
+            lagt inn.
+          </p>
+          <p>
+            Obs! Det er bare du som kan lage ordsky. Det betyr at om du forlater
+            denne siden så vil det ikke være mulig å lage en ordsky av ordene
+            som er lagt inn.
+          </p>
+        </TextContainer>
+      )}
       <Title>Kode: {id.toUpperCase()}</Title>
       {hasEntries ? (
         <span>{`${numberOfEntries} har lagt inn ord.`}</span>
