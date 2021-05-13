ec7ea8a77f86e5ee974269c1118d2d44f9409d05
Simon Opheim <simon.opheim@gmail.com>
2021-05-13 13:40:13 +0200

    refactor: Make header a styled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense, lazy, useEffect } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { CloudProvider } from './context/cloud/cloudContext';
 import { Home } from './pages/Home';
 import { FormsProvider } from './context/form/formsContext';
@@ -10,6 +10,7 @@ import { TextForm } from './pages/TextForm';
 import { About } from './pages/About';
 import { TermsBanner } from './components/TermsBanner';
 import { SessionPage } from './features/session/SessionPage';
+import { OrdskyHeader } from './components/molecules/Header';
 
 const Visualization = lazy(() => import('./pages/Visualization'));
 const SHOW_TERMS_KEY = 'displayedTerms';
@@ -32,32 +33,7 @@ const App: React.FC = function App() {
   };
   return (
     <Router>
-      <header className="header">
-        <h1 className="header__title">
-          <Link to="/">Ordsky.no</Link>
-        </h1>
-        <nav className="nav">
-          <Link
-            to="/form-input"
-            className="button button--small"
-            style={{ margin: '0 5px' }}
-            role="button"
-          >
-            Ny Ordsky
-          </Link>
-          <Link
-            to="/session"
-            className="button button--small"
-            style={{ margin: '0 5px' }}
-            role="button"
-          >
-            Lag en felles ordsky
-          </Link>
-          <Link to="/about" className="nav__link">
-            Om Ordsky.no
-          </Link>
-        </nav>
-      </header>
+      <OrdskyHeader />
       <main className="main-container">
         <CloudProvider>
           <FormsProvider>
