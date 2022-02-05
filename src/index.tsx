9bd66907c79ea38b6206909608e2f71532b478c8
Simon Opheim <simon.opheim@fjellsport.no>
2022-02-05 13:47:41 +0100

    remove CRA service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
-import { App } from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import { App } from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,10 +10,5 @@ ReactDOM.render(
       <App />
     </Router>
   </React.StrictMode>,
-  document.querySelector('#root')
+  document.querySelector("#root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
