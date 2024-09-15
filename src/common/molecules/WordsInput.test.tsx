6553a5004d6ac7e56ee5285f96d656ec5025ee6f
Simon Opheim <simon.opheim@fjellsport.no>
2024-09-15 15:50:59 +0200

    fix wordsinput tests by await for clicks to finish

--- src/common/molecules/WordsInput.test.tsx
+++ src/common/molecules/WordsInput.test.tsx
@@ -1,24 +1,25 @@
+import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event'
-import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
 import { WordsInput } from './WordsInput';
 
 describe('WordsInput component', () => {
   it('when clicking "legg til et nytt ord", then one additional input is visible', async () => {
     // Arrange
-    render(
-      <WordsInput title="124124" onSubmit={jest.fn} onQuit={jest.fn} />
-    );
+    render(<WordsInput title="124124" onSubmit={jest.fn} onQuit={jest.fn} />);
 
     const button = await screen.getByText(/legg til et nytt ord/i);
-    const fieldsBefore = await screen.getAllByPlaceholderText(/skriv inn et ord/i);
+    const fieldsBefore =
+      await screen.getAllByPlaceholderText(/skriv inn et ord/i);
     expect(fieldsBefore.length).toBe(1);
 
     // Act
-    userEvent.click(button);
+    await userEvent.click(button);
 
     // Assert
-    const fieldsAfter = await screen.getAllByPlaceholderText(/skriv inn et ord/i);
+    const fieldsAfter =
+      await screen.getAllByPlaceholderText(/skriv inn et ord/i);
 
     expect(fieldsAfter.length).toBe(2);
   });
@@ -29,20 +30,22 @@ describe('WordsInput component', () => {
 
     const mockSubmit = jest.fn();
 
-   render(
+    render(
       <WordsInput title="124124" onSubmit={mockSubmit} onQuit={jest.fn} />
     );
 
     const addFieldsButton = await screen.getByText(/legg til et nytt ord/i);
     const submitButton = await screen.getByText(/send inn ord/i);
-    const fieldsBefore = await screen.getAllByPlaceholderText(/skriv inn et ord/i);
+    const fieldsBefore =
+      await screen.getAllByPlaceholderText(/skriv inn et ord/i);
 
     // Act
     fireEvent.change(fieldsBefore[0], { target: { value: words[0] } });
-    userEvent.click(addFieldsButton);
-    const fieldsAfter = await screen.getAllByPlaceholderText(/skriv inn et ord/i);
+    await userEvent.click(addFieldsButton);
+    const fieldsAfter =
+      await screen.getAllByPlaceholderText(/skriv inn et ord/i);
     fireEvent.change(fieldsAfter[1], { target: { value: words[1] } });
-    userEvent.click(submitButton);
+    await userEvent.click(submitButton);
 
     // Assert
     expect(mockSubmit).toHaveBeenCalledWith(words);
