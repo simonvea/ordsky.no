623cbc5f0dd3cada7aa46cddd0a10939017ec471
Simon Opheim <simon.opheim@fjellsport.no>
2025-01-02 22:21:37 +0100

    make service functions instead of class

--- src/felles/collect/services/CollectService.ts
+++ src/felles/collect/services/CollectService.ts
@@ -1,6 +1,6 @@
-import { Session } from 'react-router';
 import { Cloud, WordCount } from '../../../common/core/cloud.types';
-import { generateId } from '../../../common/core/session';
+import { createCloud, createCloudSvg } from '../../../common/core/createCloud';
+import { wordCountToCloudInput } from '../../../common/core/wordCountToCloudInput';
 
 export type SessionState = {
   id: string;
@@ -8,52 +8,76 @@ export type SessionState = {
   wordCount?: WordCount;
   words: string[];
   numberOfEntries: number;
+  createdAt: string;
+  updatedAt: string;
 };
 
-export interface CollecService {
-  startSession: () => Promise<SessionState>;
+const baseUrl = '/api/felles';
 
-  getSession: (id: string) => Promise<SessionState>;
+export const getSession = async (id: string): Promise<SessionState> => {
+  const response = await fetch(`${baseUrl}/${id}`);
 
-  saveWords: ({}: { id: string; words: string[] }) => Promise<SessionState>;
-
-  createCloud: (
-    id: string
-  ) => Promise<SessionState & { cloud: Cloud[]; wordCount: WordCount }>;
-}
-
-export class CollectService implements CollecService {
-  async startSession(): Promise<SessionState> {
-    return { id: generateId(), words: [], numberOfEntries: 0 };
+  if (!response.ok) {
+    console.error('Error fetching session', response);
+    throw new Error('404');
   }
 
-  async getSession(id: string): Promise<SessionState> {
-    return new Promise((res, rej) => {
-      setTimeout(() => {
-        res({ id, words: [], numberOfEntries: 0 });
-      }, 2000);
-    });
-  }
+  return response.json();
+};
 
-  async saveWords({
-    id,
-    words,
-  }: {
-    id: string;
-    words: string[];
-  }): Promise<SessionState> {
-    return { id, words, numberOfEntries: 1 };
-  }
+export const saveWords = async ({
+  id,
+  words,
+}: {
+  id: string;
+  words: string[];
+}): Promise<SessionState> => {
+  const response = await fetch(`${baseUrl}/${id}/words`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(words),
+  });
+  return response.json();
+};
 
-  async createCloud(
-    id: string
-  ): Promise<SessionState & { cloud: Cloud[]; wordCount: WordCount }> {
-    return {
-      id,
-      numberOfEntries: 1,
-      cloud: [],
-      words: ['test'],
-      wordCount: [{ text: 'test', count: 0 }],
-    };
-  }
-}
+export const getWordsAndCreateCloud = async (
+  id: string
+): Promise<SessionState & { cloud: Cloud[]; wordCount: WordCount }> => {
+  const { words } = await getSession(id);
+
+  const counted = new Set();
+  let wordCount: WordCount = [];
+
+  words.forEach((w) => {
+    const word = w.toUpperCase();
+
+    if (counted.has(word)) {
+      wordCount = wordCount.map((count) => ({
+        text: count.text,
+        count: count.text === word ? count.count + 1 : count.count,
+      }));
+    } else {
+      wordCount.push({
+        text: word,
+        count: 1,
+      });
+      counted.add(word);
+    }
+  });
+
+  const sortedWordCount = wordCount.sort((a, b) => b.count - a.count);
+
+  const cloudInput = wordCountToCloudInput(sortedWordCount);
+
+  const cloud = await createCloud(cloudInput);
+
+  const svg = createCloudSvg(cloud);
+
+  const response = await fetch(`${baseUrl}/${id}/cloud`, {
+    method: 'PUT',
+    body: JSON.stringify(svg),
+  });
+  return response.json();
+};
