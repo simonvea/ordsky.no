c5285a29b6763d6ccc5a7699b1f7c75c655f577c
Simon Opheim <simon.opheim@fjellsport.no>
2025-01-05 16:02:16 +0100

    fix test runner error
    missing TextEncoder for some reason. This should fix it, as it is mostly caused by js-dom being out of date

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,1 +1,7 @@
 import '@testing-library/jest-dom';
+
+if (!globalThis.TextEncoder || !globalThis.TextDecoder) {
+  const { TextDecoder, TextEncoder } = require('node:util');
+  globalThis.TextEncoder = TextEncoder;
+  globalThis.TextDecoder = TextDecoder;
+}
