b8136bd64489c71ef624a35fef4c58866df44cc6
Simon Opheim <simon.opheim@fjellsport.no>
2025-01-05 14:05:37 +0100

    Add buttons to all formats on home

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -2,8 +2,57 @@ import React from 'react';
 import { Container } from '../common/atoms/Container';
 import { TextContainer } from '../common/atoms/TextContainer';
 import { NavButton } from '../common/atoms/NavButton';
+import styled, { keyframes } from 'styled-components';
+import { Button } from '../common/atoms/Button';
+
+const fadeInUp = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+const AnimatedNavWrapper = styled.div<{ $delay: number }>`
+  opacity: 0;
+  animation: ${fadeInUp} 0.3s ease forwards;
+  animation-delay: ${(props) => props.$delay}s;
+  flex: 1;
+  display: flex;
+  > a {
+    width: 100%;
+  }
+`;
+
+const ActionContainer = styled.section`
+  display: flex;
+  justify-content: space-between;
+  gap: 1rem;
+  align-items: stretch;
+  width: 100%;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const TransitionContainer = styled.div`
+  grid-row: 1;
+  grid-column: 1;
+`;
+
+const GridWrapper = styled.div`
+  margin-top: 2rem;
+  display: grid;
+  min-height: 200px;
+`;
 
 export const Home: React.FC = function Home() {
+  const [showOptions, setShowOptions] = React.useState(false);
+
   return (
     <>
       <TextContainer>
@@ -18,9 +67,29 @@ export const Home: React.FC = function Home() {
           </p>
         </article>
       </TextContainer>
-      <Container>
-        <NavButton to="words">Lag en ordsky</NavButton>
-      </Container>
+      <GridWrapper>
+        <TransitionContainer>
+          {showOptions ? (
+            <ActionContainer>
+              <AnimatedNavWrapper $delay={0.1}>
+                <NavButton to="text">Lag en ordsky fra tekst</NavButton>
+              </AnimatedNavWrapper>
+              <AnimatedNavWrapper $delay={0}>
+                <NavButton to="words">Lag en ordsky fra ord</NavButton>
+              </AnimatedNavWrapper>
+              <AnimatedNavWrapper $delay={0.2}>
+                <NavButton to="felles">Lag en ordsky sammen</NavButton>
+              </AnimatedNavWrapper>
+            </ActionContainer>
+          ) : (
+            <Container>
+              <Button onClick={() => setShowOptions(true)}>
+                Lag en ordsky
+              </Button>
+            </Container>
+          )}
+        </TransitionContainer>
+      </GridWrapper>
     </>
   );
 };
